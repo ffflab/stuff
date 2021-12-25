a6c14e2596b36ae35268b665ebf5ced3358c64d2
Masahiro Fujimoto <f21u.labs@gmail.com>
2021-12-25 22:39:54 +0900

    fix?

--- install/python.sh
+++ install/python.sh
@@ -8,7 +8,5 @@ echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
 echo 'eval "$(pyenv init --path)"' >> ~/.profile
 echo 'if command -v pyenv >/dev/null; then eval "$(pyenv init -)"; fi' >> ~/.bashrc
 
-exec $SHELL
-
-pyenv install anaconda3-2021.11
-pyenv global anaconda3-2021.11
+$HOME/.pyenv/bin/pyenv install anaconda3-2021.11
+$HOME/.pyenv/bin/pyenv global anaconda3-2021.11
