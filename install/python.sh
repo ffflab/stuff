165812568291738a2133e436dba7e097f9b9d6b9
Masahiro Fujimoto <f21u.labs@gmail.com>
2021-12-25 22:34:33 +0900

    install python

---
+++ install/python.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
+echo 'eval "$(pyenv init --path)"' >> ~/.profile
+echo 'if command -v pyenv >/dev/null; then eval "$(pyenv init -)"; fi' >> ~/.bashrc
+
+exec $SHELL
+
+pyenv install anaconda3-2021.11
+pyenv global anaconda3-2021.11
