8e9e2af2989525f7b38fe045595217f20c2a3ca7
M Fujimoto <96472891+ffflab@users.noreply.github.com>
2021-12-30 01:10:28 +0900

    Create gh-ubuntu.sh

---
+++ install/gh-ubuntu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#set -eux
+set -euxo pipefail
+
+#
+# curl https://raw.githubusercontent.com/ffflab/stuff/master/install/gh-ubuntu.sh | bash
+#
+
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh
